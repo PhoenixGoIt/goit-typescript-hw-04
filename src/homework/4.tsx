bafb8593a60f343b2e1c0841947551c5a82e8fa1
Lukyanchuk Ignat <115699352+PhoenixGoIt@users.noreply.github.com>
2024-05-07 12:21:44 +0300

    done

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -5,24 +5,31 @@ type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
-
+type MenuSelected = {
+  selectedMenu: Menu | {}
+}
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
 });
 
 // Додайте тип MenuAction
+type MenuAction = {
+  onSelectedMenu: typeof noop
+}
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode
 };
-
+type SelectedMenu = {
+  id: number | null
+}
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({id: null});
 
   const menuContextAction = useMemo(
     () => ({
@@ -48,7 +55,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: [Menu] // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
